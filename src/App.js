191f18b7551a14eafe41a012e57e8c383e3c0c44
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-12-12 13:43:36 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import Header from "./header/Header";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
+import Shop from "./shop/Shop";
+import Cart from "./cart/Cart";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
