d38cada62207cf7be4076cf3bb770aaa5e1d1f13
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-12-12 13:07:01 +0200

    Add browser router to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import "./App.css";
+import Header from "./header/Header";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <div className="App">Clean App</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
