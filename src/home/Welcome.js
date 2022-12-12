b781151728824c0b508daa3ad2915cbcb3324074
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-12-12 13:53:16 +0200

    Create welcome component

---
+++ src/home/Welcome.js
@@ -0,0 +1,11 @@
+function Welcome() {
+  return (
+    <div className="Welcome">
+      <h2 className="big">
+        Welcome to our bikers family <br /> let's get rolling!
+      </h2>
+    </div>
+  );
+}
+
+export default Welcome;
