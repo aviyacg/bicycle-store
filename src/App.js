bf92cade5a489d0a143074a8677a3f5b865e58f2
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-12-13 17:54:03 +0200

    Add cart state logic

--- src/App.js
+++ src/App.js
@@ -6,8 +6,43 @@ import Shop from "./shop/Shop";
 import Cart from "./cart/Cart";
 
 import blueBike from "./assets/images/blue-bike.jpg";
+import { useState } from "react";
 
 function App() {
+  const [cart, setCart] = useState({});
+
+  const addItem = (item) => {
+    const prevItem = cart[item.name];
+    if (prevItem) {
+      prevItem.quantity = prevItem.quantity + 1;
+      setCart({ [item.name]: prevItem, ...cart });
+      console.log("add 1 more item", item);
+    } else {
+      setCart({
+        [item.name]: {
+          item: { name: item.name, price: item.price },
+          quantity: 1,
+        },
+        ...cart,
+      });
+      console.log("add new item", { item });
+    }
+  };
+
+  const removeItem = (item) => {
+    const prevItem = cart[item.name];
+    if (prevItem) {
+      prevItem.quantity = prevItem.quantity - 1;
+      if (prevItem.quantity > 0) {
+        setCart({ [item.name]: prevItem, ...cart });
+      } else {
+        const prevCart = cart;
+        delete prevCart[item.name];
+        setCart(prevCart);
+      }
+    }
+  };
+
   const itemList = [
     {
       name: "Sky Blue Bike",
@@ -24,8 +59,16 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop itemList={itemList} />} />
-          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/shop"
+            element={<Shop itemList={itemList} addItem={addItem} />}
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart cart={cart} addItem={addItem} removeItem={removeItem} />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
