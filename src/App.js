a9d94f424072327fb777c96a7b46bc1454a998a8
Aviya CG <aviyacohen.tchvfvi@gmail.com>
2022-12-13 12:59:09 +0200

    Add itemList

--- src/App.js
+++ src/App.js
@@ -5,14 +5,26 @@ import Home from "./home/Home";
 import Shop from "./shop/Shop";
 import Cart from "./cart/Cart";
 
+import blueBike from "./assets/images/blue-bike.jpg";
+
 function App() {
+  const itemList = [
+    {
+      name: "Sky Blue Bike",
+      image: blueBike,
+      description:
+        "This is a really good bike to purchase and you will also pay for our rent",
+      price: 100,
+    },
+  ];
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
+          <Route path="/shop" element={<Shop itemList={itemList} />} />
           <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
